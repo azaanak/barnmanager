a50512b5036be9b52e455ad110b1601c9fb41a8c
Imran Ali <azaanak@gmail.com>
2019-05-25 14:15:42 -0400

    test1

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.900870bb0f368c106344097b80db2cc4.js"
 );
 
 self.addEventListener('message', (event) => {
